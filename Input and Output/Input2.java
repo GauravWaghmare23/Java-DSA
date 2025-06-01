4b4401bad35c534f85268b0a685611376c2909e1
[Gaurav Waghmare] <gauravwaghmare95032@gmail.com>
2025-06-01 23:07:46 +0530

    input2

--- Input and Output/Input2.java
+++ Input and Output/Input2.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Input2 {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.print("Please enter your full name :: ");
-        String name = scanner.nextLine();
+        String name = input.nextLine();
 
         System.out.print("Please enter your roll number :: ");
-        int rollno = scanner.nextInt();
+        int rollno = input.nextInt();
 
 
         System.out.println("Your name is " + name);
         System.out.println("Your roll no. is " + rollno);
         
-        scanner.close();
+        input.close();
     };
 };
