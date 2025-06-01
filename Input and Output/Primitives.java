4f3f7577b9c8857a4000db3116b507115d915fd2
[Gaurav Waghmare] <gauravwaghmare95032@gmail.com>
2025-06-01 22:02:08 +0530

    primitives

---
+++ Input and Output/Primitives.java
@@ -0,0 +1,16 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 43;
+        float f= 6.3f;
+        char letter = '4';
+        long largeNumber = 456787654345678765L;
+        double largeDecimal = 213456432.21332;
+        boolean RightWrong = true;
+        System.out.println(rollno);
+        System.out.println(f);
+        System.out.println(letter);
+        System.out.println(largeNumber);
+        System.out.println(largeDecimal);
+        System.out.println(RightWrong);
+    }
+}
